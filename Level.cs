c4d887e0af951e8a57560184b747c85a49b44cf3
Ermine <ermux12@gmail.com>
2020-08-15 16:29:15 +0300

    version start

---
+++ Level.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnotherPacMan
+{
+    class Level:PictureBox
+    {
+        public Level()
+        {
+            InitalizeLevel();
+        }
+
+        private void InitalizeLevel()
+        {
+            this.BackColor = Color.DeepSkyBlue;
+            this.Size = new Size(400, 400);
+            this.Location = new Point(40, 30);
+            this.Name = "Level";
+        }
+    }
+}
